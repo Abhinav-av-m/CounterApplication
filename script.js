0281d783f18baa16d69205d03081d2d3a022b5e7
Abhinav-av-m <connectabhinavav@gmail.com>
2024-11-13 15:30:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let count = 0;
+let maxLimit = 9;
+const displayMessage = function (text) {
+  document.querySelector(".text").textContent = text;
+};
+const display = function (counterLabel) {
+  document.querySelector(".counterLabel").textContent = counterLabel;
+};
+// Increase Button
+document.querySelector(".increaseBtn").addEventListener("click", function () {
+  if (count < maxLimit) {
+    count++;
+    display(count);
+    displayMessage("");
+  } else if (count >= maxLimit) {
+    displayMessage("exceed limit");
+    display("");
+  }
+});
+// Decrease Button
+document.querySelector(".decreaseBtn").addEventListener("click", function () {
+  if (count > 0) {
+    count--;
+    display(count);
+    displayMessage("");
+
+  } else if (count <= 0) {
+    displayMessage("Below Zero");
+    display("");
+  }
+});
+// Reset Button
+document.querySelector(".resetBtn").addEventListener("click", function () {
+  count = 0;
+  display(count);
+  displayMessage("");     
+});
